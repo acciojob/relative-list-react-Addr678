4f02b844f35e03fb59a5455751cfbb6fd7b97847
Addr678 <im_yadavprashantkumar33@gmail.com>
2023-07-19 16:56:01 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
-}
+const relativeList = [
+{ key: 'relativeListItem1', name: 'John' },
+{ key: 'relativeListItem2', name: 'Emily' },
+{ key: 'relativeListItem3', name: 'Michael' },
+{ key: 'relativeListItem4', name: 'Sophia' },
+{ key: 'relativeListItem5', name: 'Daniel' },
+];
 
-export default App
+return (
+<div id="main">
+<h1>Diwali Shopping List</h1>
+<ol key="relativeList">
+{relativeList.map((relative) => (
+<li key={relative.key}>{relative.name}</li>
+))}
+</ol>
+</div>
+);
+};
+
+export default App;
